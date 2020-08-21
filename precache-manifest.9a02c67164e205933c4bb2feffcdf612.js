3fc94bb355af1e84b8cc174105e29644fea749a0
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-21 18:36:28 +0300

    Updates

---
+++ precache-manifest.9a02c67164e205933c4bb2feffcdf612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ac28f2a7256838d9261a5207a05c28",
+    "url": "/SamuraiWayIncrement/index.html"
+  },
+  {
+    "revision": "1151429ca8bdeb9d4f94",
+    "url": "/SamuraiWayIncrement/static/css/main.9b81df09.chunk.css"
+  },
+  {
+    "revision": "9b84dc7078378249bcf2",
+    "url": "/SamuraiWayIncrement/static/js/2.6893dff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SamuraiWayIncrement/static/js/2.6893dff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1151429ca8bdeb9d4f94",
+    "url": "/SamuraiWayIncrement/static/js/main.2ca9d416.chunk.js"
+  },
+  {
+    "revision": "41f5bb6ff9a8f8eea6d7",
+    "url": "/SamuraiWayIncrement/static/js/runtime-main.c7c84ce4.js"
+  }
+]);
